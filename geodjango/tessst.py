f3da3da8787ad5e666b03cb63c70f3e422f71775
unknown <Hyperb0lA@yandex.ru>
2024-07-13 20:15:32 +0300

    Обновление

--- geodjango/tessst.py
+++ geodjango/tessst.py
@@ -16,11 +16,11 @@ from world.views import last_rainfall
 
 # Инициализируем RequestFactory и создаем тестовый запрос
 factory = RequestFactory()
-request = factory.get('/api/last_rainfall/41.50461563024414/44.9626747549759/')
+request = factory.get('/api/last_rainfall/')
 
 # Примерные координаты
-latitude = 41.50461563024414
-longitude = 44.9626747549759
+latitude = 41.917648315429695
+longitude = 45.04356967657023
 
 # Вызов функции
 response = last_rainfall(request, latitude, longitude)
--- geodjango/world/views.py
+++ geodjango/world/views.py
@@ -16,8 +16,9 @@ def last_rainfall(request, latitude, longitude):
     # point = Point(float(longitude), float(latitude)) Тут неправильные ввод переменных, они должны быть наоборот, верните мне мои 3 дня жизни))
     point = Point(float(latitude), float(longitude))
     station = get_object_or_404(WeatherStation, location__distance_lte=(point, D(km=5)))  # Найти станцию в пределах 5 км от точки
-    last_rain = WeatherData.objects.filter(station=station).order_by('-date').first()
+    last_rain = WeatherData.objects.filter(station=station).order_by("-date").first()
 
+    print(station.location)
     if last_rain:
         data = {
             'station': station.name,
@@ -27,15 +28,15 @@ def last_rainfall(request, latitude, longitude):
     else:
         data = {
             'station': station.name,
-            'date': None,
-            'rainfall': None,
         }
-    
-
-
-from django.shortcuts import render
-
-
+    return JsonResponse(data)
 
 def home(request):
-    return render(request, r"home.html")
+    # Это работа над ошибками, я пытался понять, почему он не выводит данные о станции, хотя в test работает, создал поэтому шаблон
+    # Думал, что JSON неправильный шаблон отправлял, какой же я...
+    initial_data = {
+        'station_name': 'Station6',
+        'last_rainfall': 9.0,
+        'date': '2024-07-13 00:00:00'
+    }
+    return render(request, 'home.html', {'data': initial_data})
