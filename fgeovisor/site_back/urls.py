75c840fd2ab37fb58706cf40a4cc3ad14852377e
agrovain <nezhentsev2017@gmail.com>
2024-10-20 00:40:31 +0300

    autorization and registration pages

--- fgeovisor/site_back/urls.py
+++ fgeovisor/site_back/urls.py
@@ -5,10 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MapView.as_view(), name="map"),
-    path('sign-in/', views.RegistrationView.as_view(), name="sign-in"),
-    path('log-in/', views.LoginView.as_view(), name='log-in'),
-    path('log-out/', views.logoutView, name='log-out'),
-
-
+    path('map/', views.mapView),
+    path('map/login/',views.loginView),
+    path('map/registration/',views.regView)
 ]
