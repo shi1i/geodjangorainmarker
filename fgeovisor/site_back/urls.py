4f0326bd099274f568fbda462eb543584020348a
shi1i <Hyperb0lA@yandex.ru>
2024-10-20 12:19:03 +0300

    merge from frontend
    + login and registr page

--- fgeovisor/site_back/urls.py
+++ fgeovisor/site_back/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('map/', views.MapView.as_view(), name="map"),
-    path('sign-in/', views.RegistrationView.as_view(), name="sign-in")
+    path('map/', views.mapView),
+    path('map/login/',views.loginView),
+    path('map/registration/',views.regView)
 ]
