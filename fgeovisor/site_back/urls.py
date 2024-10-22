6f81e95f993759bbb3442e77781f9f4bc0273b94
shi1i <Hyperb0lA@yandex.ru>
2024-10-22 15:09:21 +0300

    name for urls

--- fgeovisor/site_back/urls.py
+++ fgeovisor/site_back/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('map/', views.MapView.as_view()),
+    path('map/', views.MapView.as_view(), name="map"),
     path('sign-in/', views.RegistrationView.as_view(), name="sign-in")
 ]
